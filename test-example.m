5661aa5fc8e8d24266a76f731eb8a40d7305fe5b
Eric Yip <smartye_uestc@hotmail.com>
2019-01-15 23:00:52 +0800

    Add files via upload

---
+++ test-example.m
@@ -0,0 +1,20 @@
+clc;clear;
+
+%% input A: the adjacency matrix
+
+option.maxiter = 1000;
+option.tolfun = 5e-4;
+option.maxiter_pre = 500;
+option.verbose = 1;
+option.UpdateVi = 0; 
+option.lambda = 1;
+
+layers = [256, 128, 36];
+
+p = numel(layers);
+
+%% Deep AE NMF
+[U, V, dnorm, dnormarray] = DANMF(A, layers, option);
+Vp = V{p}';
+
+%% Vp captures the community memberships
